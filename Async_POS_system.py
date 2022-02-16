e03c6ad7f3e68a6d48c762baff2fcf879e96deb3
Tuan Hoang Nguyen <nguyentuanhoang72@gmail.com>
2022-02-16 11:07:57 +1100

    Add prompt of program to Test.py

---
+++ Async_POS_system.py
@@ -0,0 +1,11 @@
+class Inventory:
+    async def get_number_of_items():
+        pass
+    async def get_catalogue():
+        pass
+    async def get_stock(item_id):
+        pass
+    async def decrement_stock(item_id):
+        pass
+    async def get_item(item_id):
+        pass
---
+++ Test.py
@@ -0,0 +1,34 @@
+"""
+Combos: any combination of any burger, side and drink 
+    _most expensive items are added to combos first
+
+display_catalogue(catalogue): output all items in the catalogue
+    _in main.py
+
+show_catalogue(catalogue): display all item_id in the catalogue
+
+User Interface:
+    Start: by welcoming the customer and displaying the cataloggue
+
+    User will be prompted to enter one item number at a time and 
+    have their input validated
+        _no delay that user have to wait before adding another
+        item to their order
+    
+    Place their order by entering "q"
+        _validate that there is sufficient stock of the items
+        and inform them if an item is out of stock
+
+    Order passed: create any possible combos and display an 
+    order summary 
+        _(outline combo and their price as well
+        as individual item)
+
+    Display subtotal, tax and total (subtotal + tax)
+        _tax rate = 5%
+    
+    Confirm they would like to purchase this order
+        _After purchasing or declining purchase, program
+        should ask them if they would like to order again
+
+"""
